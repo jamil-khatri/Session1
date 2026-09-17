e4e0cbe674f0f2e772dc3e1b493dd21b12868859
jamil-khatri <jkhatri37capp@gmail.com>
2026-09-17 14:24:40 +0530

    Create app_use_cases.py

---
+++ app_use_cases.py
@@ -0,0 +1,3 @@
+print("Instagram - Python can be used for data analysis and recommendation systems.")
+print("WhatsApp - Python can be used for analyzing messages and improving spam detection.")
+print("YouTube - Python can be used for analyzing user activity and recommending videos.")
